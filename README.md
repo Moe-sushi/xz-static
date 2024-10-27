f860d99e42f6990a634fc9f1a1bca5542f8f15e6
Moe-hacker <moe-hacker@outlook.com>
2024-10-27 21:10:53 +0800

    x

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# xz-static
-xz static binary for aarch64, armv7, x86_64, i386 and riscv64
+xz static binary
---
+++ build.sh
@@ -0,0 +1,22 @@
+rm /etc/resolv.conf
+echo nameserver 1.1.1.1 > /etc/resolv.conf
+echo https://dl-cdn.alpinelinux.org/alpine/edge/testing >> /etc/apk/repositories
+apk add wget make clang git xz-dev libintl libbsd-static libsemanage-dev libselinux-utils libselinux-static xz-libs zlib zlib-static libselinux-dev linux-headers libssl3 libbsd libbsd-dev gettext-libs gettext-static gettext-dev gettext python3 build-base openssl-misc openssl-libs-static openssl zlib-dev xz-dev openssl-dev automake libtool bison flex gettext autoconf gettext sqlite sqlite-dev pcre-dev wget texinfo docbook-xsl libxslt docbook2x musl-dev gettext gettext-asprintf gettext-dbg gettext-dev gettext-doc gettext-envsubst gettext-lang gettext-libs gettext-static
+apk add upx
+mkdir output
+
+# OK
+git clone https://github.com/xz-mirror/xz
+cd xz
+autoreconf -fi
+./autogen.sh
+./configure LDFLAGS="-static" --enable-static --disable-shared
+make -j8 LDFLAGS="--static"
+strip src/xz/xz
+upx src/xz/xz
+cp src/xz/xz ../output/
+cp COPYING ../output/LICENSE-xz
+cd ..
+cd output
+tar -cvf ../$(uname -m).tar .
+exit 0
